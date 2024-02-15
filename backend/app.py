ad7e349aa7c9b551410c180fa5874602823f1531
Jewell Anne Diamante <jewellanneddiamante@iskolarngbayan.pup.edu.ph>
2024-02-15 15:41:52 +0800

    updated backend

--- backend/app.py
+++ backend/app.py
@@ -63,4 +63,4 @@ def map_endpoint():
         return jsonify({'error': 'Internal Server Error'}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    app.run(host="0.0.0.0", port=8000)
